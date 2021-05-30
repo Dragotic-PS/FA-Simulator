c69d913f127f675afca4b3bfa802318f631c238f
Dragotic-PS <pokemon.ahmed@gmail.com>
2021-05-30 13:28:08 +0500

    Add Visualization + README

---
+++ README.md
@@ -0,0 +1,24 @@
+# FA Simulator
+A DFA simulator for quickly generating acceptable strings and plotting the DFA. Helps build intuition for how state machines, deterministic finite automatons in particular, work.
+
+## Installation
+
+```
+$ pip install pysimpleautomata tabulate statemachine
+```
+
+You also need to install [GraphViz](http://graphviz.org//) and set it on your system-path
+
+## Dependencies
+
+### PySimpleAutomata
+Used for graphical representation of the FA simulated. Requires GraphViz.
+
+### Tabulate
+Used for CLI table to showcase accepted and rejected strings
+
+### StateMachine
+Used for state machine simulations. The wrapper was built around this.
+
+## Author
+[Dragotic-PS](https://github.com/Dragotic-PS)
--- index.py
+++ index.py
@@ -1,12 +1,22 @@
 # Shaheer Ahmed (k190233) - 4F - ToA A3 - Q5
 
 from statemachine import StateMachine, State, Transition
+import json
+from PySimpleAutomata import automata_IO
+from tabulate import tabulate
 
 class FiniteAutomata:
 	def __init__(self):
 		self.states = {}
 		self.transitions = {}
 		self.initialState = None
+		self.jsonDump = {
+			"alphabet": [],
+			"states": [],
+			"initial_states": [],
+			"accepting_states": [],
+			"transitions": [],
+		}
 
 	def transition(self, fromState = None, token = None):
 		# TODO: null checks
@@ -54,26 +64,82 @@ class FiniteAutomata:
 
 		return None
 
+	def buildFA(self):
+		if len(self.states.keys()) <= 0 or len(self.transitions.keys()) <= 0:
+			return
+
+		alphabets = set()
+		
+		self.jsonDump["states"] = list(self.states.keys())
+
+		for state in self.states:
+			if self.states[state].initial == True:
+				self.jsonDump["initial_states"].append(state)
+
+			if self.states[state].value == True:
+				self.jsonDump["accepting_states"].append(state)
+
+		self.jsonDump["initial_state"] = self.jsonDump["initial_states"][0]
+
+		for fromState in self.transitions:
+			for token in self.transitions[fromState]:
+				alphabets.add(token)
+				self.jsonDump["transitions"].append([fromState, token, self.transitions[fromState][token]["nextState"]])
+
+		self.jsonDump["alphabet"] = list(alphabets)
+
+	def dumpJson(self, dumpPath = None):
+		if len(self.jsonDump.keys()) <= 0 or dumpPath is None:
+			return
+
+		with open(dumpPath, 'w') as f:
+			json.dump(self.jsonDump, f)
+
+	def getFormattedDFA(self, dfaPath = None):
+		if dfaPath is None:
+			return
+		
+		return automata_IO.dfa_json_importer(dfaPath)
+
+	def saveSVG(self, dfaPath = None, dfaName = None, svgPath = None):
+		if dfaPath is None or dfaName is None or svgPath is None:
+			return
+
+		dfa = self.getFormattedDFA(dfaPath)
+
+		automata_IO.dfa_to_dot(dfa, dfaName, svgPath)
+
+	def visualize(self, dfaPath = None, dfaName = None, svgPath = None):
+		if dfaPath is None or dfaName is None or svgPath is None:
+			return
+
+		self.buildFA()
+		self.dumpJson(dfaPath)
+		self.getFormattedDFA(dfaPath)
+		self.saveSVG(dfaPath, dfaName, svgPath)
+
 	def reset(self):
 		self.states = {}
 		self.transitions = {}
 
 	# Tests a string on our FA
-	def test(self, string = None, verbose = False):
+	def test(self, string = None, verbose = False, silent = False):
 		state = FA.initialState
 		oldState = state
 
-		print(f"Testing [string = \"{string}\"]")
-		if (verbose):
+		if not silent:
+			print(f"Testing [string = \"{string}\"]")
+		if (verbose and not silent):
 			print(f"Starting from [state = {state}]...")
 
 		for char in string:
 			state = FA.transition(fromState=state, token=char)
-			if (verbose):
+			if (verbose and not silent):
 				print(f"Transitioned from [state = {oldState}] to [state = {state}] with [token = {char}]...")
 			oldState = state
 		
-		print(f"Ended at [state = {state}] [value = \"{self.getState(state).value}\"]...")
+		if not silent:
+			print(f"Ended at [state = {state}] [value = \"{self.getState(state).value}\"]...")
 
 		return self.getState(state).value
 
@@ -128,6 +194,12 @@ FA.addTransition("q8", "q3", "b")
 FA.addTransition("q9", "q7", "a")
 FA.addTransition("q9", "q8", "b")
 
+# visualizes the DFA constructed
+FA.visualize("./dfa.json", "DFA", "./")
+
+acceptedStrings = []
+rejectedStrings = []
+
 # Generates All Possible Strings Of our FA, of specified len
 def getCombos(str = "", i = 0, len = 10):
 	if (i > len):
@@ -135,11 +207,16 @@ def getCombos(str = "", i = 0, len = 10):
 
 	# print(f"{str}")
 
-	if (FA.test(str, verbose=False)):
-		print(str)
+	if (FA.test(str, verbose=False, silent = True)):
+		# print(str)
+		acceptedStrings.append(str)
 		# print("\n")
+	else:
+		rejectedStrings.append(str)
 
 	getCombos(str + "a", i + 1, len)
 	getCombos(str + "b", i + 1, len)
 
-getCombos(len=3)
+getCombos(len=5)
+
+print(tabulate([list(item) for item in zip(acceptedStrings, rejectedStrings)], headers=["Accepted Strings", "Rejected Strings"], tablefmt="pretty"))
